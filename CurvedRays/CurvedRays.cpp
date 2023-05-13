1a97fb4e0c7f51ca4ee1c99df9686336608c33fa
Kate Ferris <kferris@fastmail.com>
2023-05-13 23:19:38 +1000

    First project commit - reviisit in LA 😁

---
+++ CurvedRays/CurvedRays.cpp
@@ -0,0 +1,9 @@
+// CurvedRays.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Experiments with procedural volume density & shading; see curvedRaysConcept.txt";
+}
